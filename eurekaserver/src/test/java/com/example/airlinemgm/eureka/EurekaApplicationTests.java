f9472839d3dd42f92c4808f7e6a7051cc27138a5
tejastakle <112059658+tejastakle@users.noreply.github.com>
2022-09-26 11:19:19 +0530

    Add files via upload

---
+++ eurekaserver/src/test/java/com/example/airlinemgm/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlinemgm.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
